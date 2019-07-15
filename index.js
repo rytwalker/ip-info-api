5944a0fc72e1837e1f91faa2eca38f7ca58c0df1
Ryan Walker <rytwalker@gmail.com>
2019-07-15 00:06:17 -0400

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const serverless = require('serverless-http');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
+
 app.get('/', function(req, res) {
-  //   res.send('Hello World!');
   res.status(200).json({ message: 'hello world' });
 });
+
+// post request that takes in a ip address and returns info about it
 
 module.exports.handler = serverless(app);
