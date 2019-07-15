2d102788d8906515254e6d9b8325cb6ad29735da
Ryan Walker <rytwalker@gmail.com>
2019-07-15 00:54:14 -0400

    ip location package added

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const cors = require('cors');
+const iplocation = require('iplocation').default;
 const app = express();
 
 app.use(cors());
 
-app.get('/', function(req, res) {
-  res.status(200).json({ message: 'hello world' });
+app.get('/', async (req, res) => {
+  const ip = '56.70.97.8';
+  try {
+    const ipData = await iplocation(ip);
+    res.status(200).json(ipData);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 // post request that takes in a ip address and returns info about it
